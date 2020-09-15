6f1f3475723c040210fc9a4cd3fc0dc94e5e229e
moonhyerin <61622127+moonhyerin@users.noreply.github.com>
2020-09-15 17:51:28 +0900

    make video cards

--- server/routes/video.js
+++ server/routes/video.js
@@ -50,6 +50,16 @@ router.post('/uploadVideo', (req, res) => {
     })
 })
 
+router.get('/getVideos', (req, res) => {
+    //비디오를 DB에서 가져와서 클라이언트에 보낸다.
+    Video.find()
+        .populate('writer')
+        .exec((err, videos) => {
+            if(err) return res.status(400).send(err);
+            res.status(200).json({ success: true, videos })
+        });       //비디오 컬렉션 안의 모든 비디오를 가져온다.
+})
+
 router.post('/thumbnail', (req, res) => {
     //썸네일 생성 및 비디오 러닝타임 가져오기
 
